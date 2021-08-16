49d54f05c68da1bd32e724a18fd784a82dff2789
Logan Brock <logan.g.brock@gmail.com>
2021-08-16 14:05:11 -0400

    login on backend works - setting up context pages

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,25 +1,64 @@
+import { useContext, useRef } from 'react';
 import './login.css';
+import { loginCall } from '../../apiCalls';
+import { AuthContext } from '../../context/AuthContext';
+import { CircularProgress } from '@material-ui/core';
 
 const Login = () => {
+	const email = useRef();
+	const password = useRef();
+	const { isFetching, dispatch } = useContext(AuthContext);
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		loginCall(
+			{ email: email.current.value, password: password.current.value },
+			dispatch
+		);
+	};
+
 	return (
 		<div className='login'>
 			<div className='loginWrapper'>
 				<div className='loginLeft'>
-					<h3 className='loginLogo'>Talk Turkey</h3>
+					<h3 className='loginLogo'>Lamasocial</h3>
 					<span className='loginDesc'>
-						Connect with dog friends and the world around you on Talk Turkey.
+						Connect with friends and the world around you on Lamasocial.
 					</span>
 				</div>
 				<div className='loginRight'>
-					<div className='loginBox'>
-						<input placeholder='Email' className='loginInput' />
-						<input placeholder='Password' className='loginInput' />
-						<button className='loginButton'>Log In</button>
+					<form className='loginBox' onSubmit={handleClick}>
+						<input
+							placeholder='Email'
+							type='email'
+							required
+							className='loginInput'
+							ref={email}
+						/>
+						<input
+							placeholder='Password'
+							type='password'
+							required
+							minLength='3'
+							className='loginInput'
+							ref={password}
+						/>
+						<button className='loginButton' type='submit' disabled={isFetching}>
+							{isFetching ? (
+								<CircularProgress color='white' size='20px' />
+							) : (
+								'Log In'
+							)}
+						</button>
 						<span className='loginForgot'>Forgot Password?</span>
 						<button className='loginRegisterButton'>
-							Create a New Account
+							{isFetching ? (
+								<CircularProgress color='white' size='20px' />
+							) : (
+								'Create a New Account'
+							)}
 						</button>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
